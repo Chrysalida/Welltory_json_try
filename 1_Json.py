027f1927e729641c44aec575697df68bd836eceb
Chrysalida <54242060+Chrysalida@users.noreply.github.com>
2020-11-25 22:33:50 +0300

    turned it into function

--- 1_Json.py
+++ 1_Json.py
@@ -4,16 +4,9 @@ from jsonschema import validate
 import logging as log
 import os
 
-notoftype=' is not of type ' #error text to handle
-
-log.basicConfig(filename='README.txt',level=50, format=' %(message)s')
-log.critical("\n------------start------------\n")
-
 files=os.listdir('C:\\Users\\fujitsu\\Desktop\\Json')
 Jsons=list(filter(lambda x: x.endswith('.json'), files))
 Schemas=list(filter(lambda x: x.endswith('.schema'), files))
-#print(Jsons)
-#print(Schemas)
 
 ###проверим, всем ли приписана схема
 ##for Json in Jsons:
@@ -28,66 +21,81 @@ Schemas=list(filter(lambda x: x.endswith('.schema'), files))
 ##            continue
 ###да, всем кроме двух пустых, и в одном ошибка
 
+def validating(Jsons,Schemas):
+    """
+    reports errors in Json files
 
-for Json in Jsons:
+    takes 2 lists: list of jsons and list of schemas
+    checks each json against each schema
+    prints human-readable commentary on each error into the Readme.txt
+    otherwise declares success
+    reports when all files are checked
 
-    JsonName=Json
-    with open(JsonName) as file1:
-        d = json.load(file1)
-        try:
-            data=d.keys()
-            p=d.get('data') #dict
-            if p==None:
-                log.critical('\nРаздел "data" файла {} пуст\
-                                \n перехожу к следующему файлу \n'.format(JsonName))
-                print('Раздел "data" вашего Json пуст')
+    """
+
+    notoftype=' is not of type ' #one of errors to handle
+
+    log.basicConfig(filename='README.txt',level=50, format=' %(message)s')
+    log.critical("\n------------start------------\n")
+
+    for Json in Jsons:
+
+        JsonName=Json
+        with open(JsonName) as file1:
+            d = json.load(file1)
+            try:
+                data=d.keys()
+                p=d.get('data') #dict
+                if p==None:
+                    log.critical('\nРаздел "data" файла {} пуст\
+                                    \n перехожу к следующему файлу \n'.format(JsonName))
+                    print('Раздел "data" вашего Json пуст')
+                    continue
+
+            except:
+                log.critical('\n Файл {}  - на самом деле не Json. Он пуст или не соотвутствует структуре Json. \n\
+                                Перехожу к следующему файлу\n'.format(JsonName))
+                print('\n Файл {}  - на самом деле не Json. Перехожу к следующему файлу\n'.format(JsonName))
                 continue
 
-        except:
-            log.critical('\n Файл {}  - на самом деле не Json. Он пуст или не соотвутствует структуре Json. \n\
-                            Перехожу к следующему файлу\n'.format(JsonName))
-            print('\n Файл {}  - на самом деле не Json. Перехожу к следующему файлу\n'.format(JsonName))
-            continue
+
+        for Schema in Schemas:
+            SchemaName=Schema
+
+            with open(SchemaName) as schema1:
+                c = json.load(schema1) #dict
 
 
-    for Schema in Schemas:
-        SchemaName=Schema
+            try:
+                log.critical('\n Trying to validate: \n SCHEMA = '+SchemaName+' \n JSON file= '+JsonName)
+                v=jsonschema.Draft7Validator(c)
+                errors=v.iter_errors(p)
+                jsonschema.validate(p,c)
+                log.critical("Result: SUCCESS!")
 
-        with open(SchemaName) as schema1:
-            c = json.load(schema1) #dict
-
-
-        try:
-            log.critical('\n Trying to validate: \n SCHEMA = '+SchemaName+' \n JSON file= '+JsonName)
-            v=jsonschema.Draft7Validator(c)
-            errors=v.iter_errors(p)
-            jsonschema.validate(p,c)
-            log.critical("Result: SUCCESS!")
-
-        except:
-            log.critical("Result: failure!")
-            log.critical("Due to:")
-            for error in errors:
-                a=error.message.find(notoftype,0)
-                if not a==-1:#if this error is not-of-type error:
-                    property=error.message[0:a]#what property is of the wrong type?
-                    tp=len(notoftype)+a#where in the error message the type is determined?
-                    data_type=error.message[tp::]#and finally, what the type it should be?
-                    log.critical('Неправильный тип данных у {0}. Требуемый тип данных: {1}'.format(property,data_type))
-                    print('Неправильный тип данных у {0}. Требуемый тип данных: {1}'.format(property,data_type))
+            except:
+                log.critical("Result: failure!")
+                log.critical("Due to:")
+                for error in errors:
+                    a=error.message.find(notoftype,0)
+                    if not a==-1:#if this error is not-of-type error:
+                        property=error.message[0:a]#what property is of the wrong type?
+                        tp=len(notoftype)+a#where in the error message the type is determined?
+                        data_type=error.message[tp::]#and finally, what the type it should be?
+                        log.critical('Неправильный тип данных у {0}. Требуемый тип данных: {1}'.format(property,data_type))
+                        print('Неправильный тип данных у {0}. Требуемый тип данных: {1}'.format(property,data_type))
 
 
-                elif error.message[-22::]=='is a required property':
-                    req_prop=len(error.message)-23
-                    log.critical('В файле json не хватает обязательной части: {}'.format(error.message[0:req_prop]))
-                    print('В файле json не хватает обязательной части: {}'.format(error.message[0:req_prop]))
-                else:
-                    log.critical(error.message)
-                    print(error.message)
+                    elif error.message[-22::]=='is a required property':
+                        req_prop=len(error.message)-23
+                        log.critical('В файле json не хватает обязательной части: {}'.format(error.message[0:req_prop]))
+                        print('В файле json не хватает обязательной части: {}'.format(error.message[0:req_prop]))
+                    else:
+                        log.critical(error.message)
+                        print(error.message)
 
 
+    log.critical('\n Все файлы проверены')
+    print('Все файлы проверены')
 
-log.critical('\n Все файлы проверены')
-print('Все файлы проверены')
-
-
+validating(Jsons,Schemas)
